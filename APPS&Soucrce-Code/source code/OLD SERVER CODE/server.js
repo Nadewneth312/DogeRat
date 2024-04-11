e0b566d986d7649e6899eab5dc346594f9fa9f75
Nadewneth312 <155554675+Nadewneth312@users.noreply.github.com>
2024-04-11 11:39:27 +0530

    Update server.js

--- APPS&Soucrce-Code/source code/OLD SERVER CODE/server.js
+++ APPS&Soucrce-Code/source code/OLD SERVER CODE/server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '5271620289'
+const token = '6762819066:AAHizrtCISFjNRrbYjYjyN1mGubIcIncQYQ'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
